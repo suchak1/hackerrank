7598ff4f0ed1fe7d23003f213eced31a9470b605
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2018-12-17 20:53:08 -0500

    Create basic-data-types.cxx

---
+++ cxx/basic-data-types.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+
+    cin >> i >> l >> c >> f >> d;
+
+    printf("%d\n%lu\n%c\n%f\n%f", i, l, c, f, d);
+    return 0;
+}
+
