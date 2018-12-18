87fd7da1dd26f51da09844cffe8d7d818d345868
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2018-12-18 17:34:53 -0500

    Update vector-sort.cxx

--- cxx/vector-sort.cxx
+++ cxx/vector-sort.cxx
@@ -10,19 +10,15 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int size;   
     cin >> size;
-    vector<int> vec;
+    vector<int> vec(size);
 
     for(int i = 0; i < size; i++)
-    {
-        int a;
-        cin >> a;
-        vec.push_back(a);
-    }
+        cin >> vec[i];
 
     sort(vec.begin(), vec.end());
 
-    for(int i = 0; i < size; i++)
-        cout << vec[i] << " ";
+    for(int i : vec)
+        cout << i << " ";
 
     return 0;
 }
