69a074504a0169cd3373d93b8e7f28bf77e96b56
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2018-12-20 21:29:47 -0500

    Update rectangle_area.cxx

--- cxx/rectangle_area.cxx
+++ cxx/rectangle_area.cxx
@@ -18,4 +18,27 @@ class RectangleArea : public Rectangle{
 };
 
 
-
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
+    return 0;
+}
